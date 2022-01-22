614ad2c3d48818f10727a43a74235f7aa95851ca
lnalmeida <l.n.almeida.ti@gmail.com>
2022-01-22 00:13:04 -0300

    versão 1.1 - funcional - Adicionado fechamento pela tecla ESC

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const buttons = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const closeButton = document.querySelector('.close-modal');
 const modalOverlay = document.querySelector('.overlay');
+let isOpen = false;
 
 
 const openModal = () => {
+    isOpen = true;
     modal.classList.remove('hidden');
     modalOverlay.classList.remove('hidden');
 }
 
 const closeModal = () => {
+    isOpen = false;
     modal.classList.add('hidden');
     modalOverlay.classList.add('hidden');
 }
@@ -19,3 +22,10 @@ const closeModal = () => {
 buttons.forEach((button) => button.addEventListener('click', openModal));
 closeButton.addEventListener('click', closeModal);
 modalOverlay.addEventListener('click', closeModal);
+
+window.addEventListener('keydown', (e) => {
+    if (e.keyCode === 27 && isOpen) {
+        closeModal();
+    }
+});
+
